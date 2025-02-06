31145abdc1ccbb684984c60609f62ed4dfdb6fd1
Chirag945 <118551445+Chirag945@users.noreply.github.com>
2025-02-06 22:01:01 +0530

    getDepth function added

--- dep.py
+++ dep.py
@@ -4,11 +4,18 @@ def get_completion(prompt):
     completion = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
-        {"role": "developer", "content": "You are a helpful assistant."},
+        {"role": "developer", "content": "You are a assistant specializing in computer science, particularly evaluation of code. Whenever you are given a task you reason clearly and unambiguously to arrive at an answer. Instead of rushing to an answer you think the problem through even if it takes more time than usual. You are not too verbose. Your answers are short with all the necessary information included. If you do not know the answer of a particular question you simply reply with 'I do not know'"},
         {"role": "user", "content": prompt}
     ]
     )
     return (completion.choices[0].message.content)
+def getDepth(code):
+    # get the depth of the code
+    # return the depth
+    prompt=f"get the depth of the code delimited by triple backticks. Depth of the code is defined as nesting level of predefined constructs. The constructs that are considered as a new level are as follows:- a loop, assignment of a variable, defining a class, defining a function/method, a conditional using if-else or a switch call. Return a single number - the depth of the given code. Do not attach any pre or post text like depth is etc.  ```{code}```"
+    ans = get_completion(prompt)
+    return int(ans)
+
 def codeDecomposition(code):
     # break code into subcodes using AST. ast in python can only be used for python files. check clang for c/c++ files
     # suggested by chatGPT
@@ -28,6 +35,8 @@ def updateSemantic(dependency,subCodesSemantic):
 def summarizeSemantic(subCodesSemantics):
     # summarize the semantics of the subcodes
     # return the summarized semantic
+
+    
 def getSemantic(code):
     storage={}
     subCodes=codeDecomposition(code)
@@ -36,7 +45,7 @@ def getSemantic(code):
         SCDepth=getDepth(SC)
         dependencies=getDependencies(SC)
         dependencySemantics=retrieveSemantics(dependencies)
-        if SCDepth<threshold:
+        if SCDepth<3:
             subCodesSemantic=LLM(SC,dependencySemantics)
         else:
             SSCSemantics=getSemantic(SC)
@@ -46,5 +55,7 @@ def getSemantic(code):
             newDependency=updateSemantic(DP,subCodesSemantic)
             storage.update(newDependency)
     codeSemantic=summarizeSemantic(subCodesSemantics)
-    return codeSemantic        
+    return codeSemantic    
+
+ 
 
