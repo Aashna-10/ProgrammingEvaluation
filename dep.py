f9651a7e7bf4fdfc2ac94598e4330eaf37b4d6d1
Chirag945 <118551445+Chirag945@users.noreply.github.com>
2025-02-13 22:11:11 +0530

    LLM, getDependecies function completed by Aashna

--- dep.py
+++ dep.py
@@ -32,6 +32,38 @@ def codeDecomposition(code):
 def getDependencies(code):
     # get the dependencies of the code
     # return a list of dependencies
+    example = """class RunsComparator implements Comparator<Player> {
+    @Override
+    public int compare(Player p1, Player p2) {
+        // Question 1: Write code for comparing/sorting runs in descending order [Total: 2 marks]
+        // Return a negative value if the first player has more runs, 
+        // a positive value if the second player has more runs, or zero if they have the same number of runs.
+        return Integer.compare(p2.getRunsScored(), p1.getRunsScored());
+    }
+    }"""
+
+    prompt = f"""
+    The aim is to get the user defined dependencies of the code delimited by triple backticks. To achieve this follow the below steps:
+    Step 1:look at the code and understand which programming language it is written in. 
+    Step 2: Keep referring to that language's documentation when finding the dependencies
+    Step 3: Analyse the code one line at a time like a human would do and keep a track of user defined variables and methods that you do not understand
+    Step 4: The user defined variables and methods you do not understand are dependencies of a code as the code cannot be comprehended without them.
+    
+    For example if I have a java code as:
+    {example}
+
+    We do not know what Player is or any of its properties. We do not know what the getRunsScored method is either so these are considered
+    as the dependencies of the above code.
+    However inbulit functions of the language such as Integer.compare() are not dependencies as we know what it does 
+    as it is included in the documentation.
+
+    Similarly find out the dependencies for the code {code}
+
+    Step 5: Return all the dependencies and the programming language in the form of a comma seperated list only. This list should strictly only contain the names of methods or 
+    class of objects it does not understand.
+    Do not return anything except for this list."""
+    ans = get_completion(prompt)
+    return ans 
 
 def retrieveSemantics(dependencies):
     # get the semantics of the dependencies
@@ -52,9 +84,19 @@ def retrieveSemantics(dependencies):
             print(f"Dependency not found in storage for {DP}")            
     return dependencySemantics
 
-def LLM(code,dependencies,subCodesSemantics=None):
+def LLM(code,dependencySemantics,subCodesSemantics=None):    
     # use the LLM to get the semantics of the code
     # return the semantics of the code
+    prompt = f""" The aim is to get the semantics of the code delimited by triple backticks
+    given the semantics of user defined dependencies and the semantics of subcodes if there are any.
+    You are required to refer to {dependencySemantics} for meaning of user defined dependencies in the code. It is a a dictionary of the dependencies and their semantics.
+    You are required to refer t{subCodesSemantics}  for the meaning of subcodes present in the code. It is a list of the semantics of the subcodes of the given code.
+    Keeping the above meanings in mind, find the semantics of {code}. 
+    The output should strictly be in a string format that contains the meaning of the code. Do not make up any answers and if 
+    you are confused just say you don't know. Do not return the semantics of user-defined dependencies and subCode smeantics.
+    """
+    ans = get_completion(prompt)
+    return ans
 
 def updateSemantic(dependency,subCodesSemantic):
     # update the semantic of the dependency using the semantic of the subcodes
